c0e2e9924ea7009f5778aead7dcb6b4b0e91dc3d
Afif Ben Rhouma <afif99benrhouma@gmail.com>
2024-05-18 13:26:58 +0100

    commit projet

---
+++ src/test/java/com/example/projetsoap/ProjetSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
